0c84c0ac531a8241733744524569809065531691
cirsium <devnull@localhost>
2010-09-08 20:11:46 +0200

    ajout de contenu pour basics01.rb

--- basics/_basics01.rb
+++ basics/_basics01.rb
@@ -3,7 +3,7 @@
 # fichier d'aide concernant la syntaxe et les structures de base de Ruby
 # auteur : Cirsium
 # créé le 07-09-2010
-# dernière édition le 07-09-2010
+# dernière édition le 08-09-2010
 
 if __FILE__ == $0 #teste si le programme est bien lancé à partir de ce fichier (exemples d'utilisation sur des librairies)
 
@@ -36,6 +36,7 @@ if __FILE__ == $0 #teste si le programme est bien lancé à partir de ce fichier
 #maDate = monTab.join('-') # équivalent de implode de php. Assemble en une chaine les parties d'un tableau avec le séparateur spécifié
 #puts maDate
 
+#--------------------
 #----- convention de nommage
 #nom_var
 #nom_methode
@@ -46,6 +47,7 @@ if __FILE__ == $0 #teste si le programme est bien lancé à partir de ce fichier
 #$nom_var_globale (s'écrit aussi en majuscule)
 #NomClasse
 #NOMCONSTANTE ou NomConstante
+#--------------------
 
 #----- if... else... end
 #nombre = gets.chomp.to_i # to_i convertit si possible une chaine en integer
@@ -99,25 +101,25 @@ if __FILE__ == $0 #teste si le programme est bien lancé à partir de ce fichier
 #	puts elt
 #end
 
-#-----each {...}
+#----- each {...}
 #tab = [1, 2, 3, 4]
 #tab.each do | elt |
 #	puts elt
 #end
 
-# syntaxe alternative
+#----- syntaxe alternative
 #tab = [1, 2, 3, 4]
 #tab.each { | elt |
 #	puts elt }
 
-# each_byte : parser lun à un les caractères d'une chaine
+#----- each_byte : parser lun à un les caractères d'une chaine
 #chaine = "ayu"
 #chaine.each_byte do |carac|
 #	ascii_sup = carac + 1
 #	puts ascii_sup.chr
 #end
 
-# each_line : parser ligne par ligne
+#----- each_line : parser ligne par ligne
 #chain = "pierre\npaul\njacques"
 #i = 1
 #chain.each_line do |ligne|
@@ -125,5 +127,101 @@ if __FILE__ == $0 #teste si le programme est bien lancé à partir de ce fichier
 #	i += 1
 #end
 
+#----- répétition d'une opération X fois
+#5.times do
+#	puts "XXX"
+#end
+
+#--------------------
+#----- sorties de boucles
+# return : sort complètement de la boucle et de la méthode qui contient la boucle
+# break : sort de la boucle, et continue le code juste après
+# next : passe directement à la prochaine itération de la boucle
+# redo : re-passe l'itération courante de la boucle
+#--------------------
+
+#----- procédures : stocker des blocs de code pour réutilisation à la demande, passage en paramètre à une méthode
+#quel_temps = Proc.new do |mon_param|
+#	puts "Il fait #{mon_param} aujourd'hui !"
+#end
+#quel_temps.call "gris"
+
+#--------------------
+#----- gestion d'erreurs
+
+#----- begin... rescue... end
+#def ouvre_fichier(nom_fichier)
+#	begin
+#		fichier = File.open(nom_fichier, "r")
+#	rescue # on va demander à l'utilisateur d'entrer lui-même le nom du fichier
+#		puts "Le fichier n'a pu etre ouvert, saisissez un nom correct"
+#		nom_fichier = gets.chomp
+#		retry #permet de relancer le bloc. attention : bouclage tant que l'utilisateur n'a pas entré un nom correct
+#	end
+#end
+#ouvre_fichier("Kapoue.txt")
+
+#----- raise
+#begin
+#	puts "Saisissez ZE nombre"
+#	nombre = gets.chomp.to_i
+#	if (nombre != 42)
+#		raise "Vous etes un minable !"
+#	end
+#rescue
+#	puts "Erreur : #{$!}" # $! est une variable globale qui retourne le dernier message d'erreur
+#ensure # Dans tous les cas, cette instruction s'exécutera
+#	puts "Ainsi va la vie..."
+#end
+#--------------------
+
+#--------------------
+#----- manipulation de fichiers
+
+#----- création de fichiers et dossiers
+#Dir::mkdir("test", 0777) # création d'un répertoire avec droits complets pour tout le monde
+#File::new("test/test.txt", "w+")
+
+#----- affichage ligne par ligne avec numéro de ligne
+#fichier = File.open("test/test.txt", "r")
+#i = 1
+#fichier.each_line do |ligne|
+#	puts "#{i} - #{ligne}"
+#	i += 1
+#end
+#fichier.close
+
+#----- écrire dans un fichier
+#mon_tab = ["Bonjour", "Ravi de vous recontrer", "Comment allez-vous ?"]
+#mon_fichier = File.open("test/test.txt", "w")
+#mon_tab.each do |elt|
+#	mon_fichier.write elt+"\n"
+#end
+#mon_fichier.close
+
+#----- renommer ou déplacer un fichier
+#File::rename("test/test.txt", "test/toto.txt") #renommage. si rép différent pour tot.txt par ex, déplacement du fichier
+
+#----- lister le contenu d'un répertoire et de tous ses sous-répertoires
+#class String
+#	def liste_rep(espacement = "")	
+#		liste_exclus = [".", ".."]
+#		d = Dir.open(self)		
+#		liste_dir = d.sort - liste_exclus	
+#		liste_dir.each do |fichier|
+#			case File.ftype(self+fichier)
+#				when "directory"
+#					puts "#{espacement} + #{fichier}/"
+#					espacement += "     "
+#					(self + fichier + "/").liste_rep(espacement)
+#					espacement = espacement[0, espacement.length-4]
+#				when "file"
+#					puts "#{espacement} - #{fichier}"
+#				end
+#			end
+#	end
+#end
+#"./".liste_rep
+#--------------------
 
 end
