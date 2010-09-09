6870b8716bd34aceccb5838b63d7e15a33d021bf
cirsium <devnull@localhost>
2010-09-09 11:23:13 +0200

    ajout de basics03.rb

---
+++ basics/_basics03.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+# fichier d'aide concernant divers aspects de Ruby
+# auteur : Cirsium
+# créé le 09-09-2010
+# dernière édition le 08-09-2010
+
+
+
+if __FILE__ == $0
+
+#----- méthodes de conversion de classe
+# String et Float vers Integer : to_i
+# Float et Integer vers String : to_s
+# String et Integer vers float : to_f
+
+#----- demander si un objet appartient à une classe particulière : objet.is_a(Classe)
+#12.is_a?(Integer)
+
+#----- couper une expression en plusieurs lignes : le dernier caractère de ligne doit être un backslash "\"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
