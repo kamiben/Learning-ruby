585655136f828b25e21f01539cd858319ffe88ef
Kamiben <devnull@localhost>
2010-09-13 21:54:26 +0200

    implémentation des url propres (url contenant l'id et le nom) pour un meilleur seo

--- diggy/app/models/story.rb
+++ diggy/app/models/story.rb
@@ -1,4 +1,9 @@
 class Story < ActiveRecord::Base
   validates_presence_of :name, :link #Validation de la présence des paramètres name et link. Sans cette validation il était possible d'entrer un enregistrement sans nom ou lien
   has_many :votes
+  
+  # Ecrase la méthode par défaut de gestion des liens pour la création de liens jolis contenant le nom de la story
+  def to_param
+    "#{id}-#{name.gsub(/\W/, '-').downcase}"
+  end
 end
