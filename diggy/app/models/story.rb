049947bcd04b3fb81f02666bfb7a98dceb2f9fc5
Kamiben <devnull@localhost>
2010-09-14 14:31:06 +0200

    création des liens entre les tables

--- diggy/app/models/story.rb
+++ diggy/app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ActiveRecord::Base
+  belongs_to :user
   validates_presence_of :name, :link #Validation de la présence des paramètres name et link. Sans cette validation il était possible d'entrer un enregistrement sans nom ou lien
   has_many :votes do
     def latest
--- diggy/db/schema.rb
+++ diggy/db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100913191524) do
+ActiveRecord::Schema.define(:version => 20100914122023) do
 
   create_table "stories", :force => true do |t|
     t.string   "name"
     t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "password"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -23,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20100913191524) do
     t.integer  "story_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
