dd695206d905c274cbad0febc41cd81901afddc6
Kamiben <devnull@localhost>
2010-09-14 15:33:25 +0200

    Ajout de la methode permettant de verifier si un utilisateur est connecté, et de celle permettant de forcer sa connexion

--- diggy/app/controllers/application_controller.rb
+++ diggy/app/controllers/application_controller.rb
@@ -9,4 +9,20 @@ class ApplicationController < ActionController::Base
     return unless session[:user_id] # Teste si le cookie de session est présent et contient un user_id (un utilisateur est donc connecté)
     @current_user = User.find_by_id(session[:user_id]) # Récupère l'objet user via son id, il sera maintenant dispo pour le controlleur appelé
   end 
+  
+  
+# Méthode pour vérifier si un utilisateur est enregistré 
+  def logged_in? 
+    ! @current_user.nil?
+  end
+  helper_method :logged_in? #On caractérise cette méthode comme helper : cela va permettre de l'utiliser de partout dans l'application 
+  
+  
+#Méthode qui permet de forcer la connexion
+  def login_required
+    return true if logged_in? # renvoie vrai si connecté 
+    session[:return_to] = request.request_uri # Envoie en parametre la page d'origine pour qu'il soit redirigé apres sa connexion
+    redirect_to new_session_path and return false #sinon redirige pour qu'il puisse se connecter
+  end
+  
 end
