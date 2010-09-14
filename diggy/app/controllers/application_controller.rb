90007dff3e79d8f2afa56cf7427e59acbf04a065
Kamiben <devnull@localhost>
2010-09-14 15:01:13 +0200

    Récupération de l'objet user (identifié) et mise à disposition pour les controlleurs avec un filtre before

--- diggy/app/controllers/application_controller.rb
+++ diggy/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :fetch_logged_in_user # Filtre : la méthode mise en lien sera systématiquement effectuée avant chaque appel de controlleur
+  
+  protected # Eviter un acces public à cette méthode
+  
+  def fetch_logged_in_user
+    return unless session[:user_id] # Teste si le cookie de session est présent et contient un user_id (un utilisateur est donc connecté)
+    @current_user = User.find_by_id(session[:user_id]) # Récupère l'objet user via son id, il sera maintenant dispo pour le controlleur appelé
+  end 
 end
