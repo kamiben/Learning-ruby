fc47fa6fe98b4a4b4b15055ad2370b7e1520a8db
Kamiben <devnull@localhost>
2010-09-14 11:59:19 +0200

    Ajout d'un historique des votes (3 au max)  avec mise a jour en ajax quand il y a vote

--- diggy/app/models/story.rb
+++ diggy/app/models/story.rb
@@ -1,9 +1,16 @@
 class Story < ActiveRecord::Base
   validates_presence_of :name, :link #Validation de la présence des paramètres name et link. Sans cette validation il était possible d'entrer un enregistrement sans nom ou lien
-  has_many :votes
+  has_many :votes do
+    def latest
+      find :all, :order => 'id DESC', :limit => 3
+    end
+  end
   
   # Ecrase la méthode par défaut de gestion des liens pour la création de liens jolis contenant le nom de la story
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
+  
+
+  
 end
