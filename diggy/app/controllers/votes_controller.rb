b5e6d8a9e7e37c2ca7d565bf9fae6593e714d7d7
Kamiben <devnull@localhost>
2010-09-14 10:04:57 +0200

    Intégration ajax de vote sur une story (spécifique rails3)

--- diggy/app/controllers/votes_controller.rb
+++ diggy/app/controllers/votes_controller.rb
@@ -1,5 +1,7 @@
 class VotesController < ApplicationController
   def create
+    @story = Story.find(params[:story_id]) # On retrouve la bonne story via son id
+    @story.votes.create # on lui ajoute un vote
   end
 
 end
