8058fcc9cfe70cb088aa1691c5762b39c5270524
Kamiben <devnull@localhost>
2010-09-14 09:17:00 +0200

    Création du controlleur pour gestion des votes et modif de la route en conséquence

--- diggy/config/routes.rb
+++ diggy/config/routes.rb
@@ -1,9 +1,12 @@
 #Shovell::Application.routes.draw do
+ # get "votes/create"
+
 #  get "stories/index"
 #  get "stories/new"
 
 ActionController::Routing::Routes.draw do |map| # Attention pas terrible pour rails3
- map.resources :stories # Inclure des resources supplémentaires pour stories dans les routes, permet donc l'acces via url à : index (utilisé), new(utilisé), create (utilisé), show, edit, update, destroy
+ map.resources :stories, :has_many => :votes # Inclure des resources supplémentaires pour stories dans les routes, permet donc l'acces via url à : index (utilisé), new(utilisé), create (utilisé), show, edit, update, destroy
+ # has_many permet quand à lui de lier la vue vote a la vue story, un vote ne sera maintenant accessible que via /story/vote et plus via /vote
  map.connect ':controller/:action/:id' # Attention pas top rail3
  map.connect ':controller/:action/:id.:format' #idem
 
