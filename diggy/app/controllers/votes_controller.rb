54fbebb04d7126296eeea6331e54f970b88c49fe
Kamiben <devnull@localhost>
2010-09-14 10:20:28 +0200

    Intégration d'une alternative pour ceux qui n'ont pas un browser compatible ajax/html5

--- diggy/app/controllers/votes_controller.rb
+++ diggy/app/controllers/votes_controller.rb
@@ -2,6 +2,18 @@ class VotesController < ApplicationController
   def create
     @story = Story.find(params[:story_id]) # On retrouve la bonne story via son id
     @story.votes.create # on lui ajoute un vote
+    
+    respond_to do |format| 
+      format.html { redirect_to @story } 
+      format.js 
+    end
   end
 
 end
+
+# Seconde possibilité d'intégration d'ajax avec moins de lignes de code : spécifique rails 3, on peut alors enlever votes.create, et tout ce qui concerne respond to
+#<div id="vote_form"> 
+#<% form_for [ @story, Vote.new ], :remote => true do |f| %> 
+#<%= f.submit 'shove it' %> 
+#<% end %> 
+#</div>
