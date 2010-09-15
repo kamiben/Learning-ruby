db0f08edaf13eaf5e6563886d513e692e43571e7
Kamiben <devnull@localhost>
2010-09-15 08:54:54 +0200

    initialisation du compteur dans la migration

--- diggy/db/migrate/20100915064509_add_coutner_cache_to_stories.rb
+++ diggy/db/migrate/20100915064509_add_coutner_cache_to_stories.rb
@@ -1,7 +1,12 @@
 class AddCoutnerCacheToStories < ActiveRecord::Migration
   def self.up
+    add_column :stories, :votes_count, :integer, :default => 0 # initialisation a zero
+    Story.find(:all).each do |s| # calcule le compteur pour toutes les story
+      s.update_attribute :votes_count, s.votes.length
+    end
   end
 
   def self.down
+    remove_column :stories, :votes_count
   end
 end
