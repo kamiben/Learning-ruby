12e5f501d618151f23e3693a04d59467cd30ae6b
Kamiben <devnull@localhost>
2010-09-15 08:59:02 +0200

    modif de vote.rb pour prendre en compte le compteur et mise a jour dans l'appel ajax pour qu'il reload l'objet pour mettre a jour le compteur

--- diggy/db/schema.rb
+++ diggy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100914122023) do
+ActiveRecord::Schema.define(:version => 20100915064509) do
 
   create_table "stories", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20100914122023) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "votes_count", :default => 0
   end
 
   create_table "users", :force => true do |t|
