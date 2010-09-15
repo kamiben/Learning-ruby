1cdb562b670ace9fefe7e406ace51cfdb8612710
Kamiben <devnull@localhost>
2010-09-15 10:36:23 +0200

    callback pour créer un vote lorsqu'on ajoute une story

--- diggy/app/models/story.rb
+++ diggy/app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ActiveRecord::Base
+  after_create :create_initial_vote # callback qui ajotue un vote apres creation d'une story
   belongs_to :user
   validates_presence_of :name, :link #Validation de la présence des paramètres name et link. Sans cette validation il était possible d'entrer un enregistrement sans nom ou lien
   has_many :votes do
@@ -12,6 +13,8 @@ class Story < ActiveRecord::Base
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
   
-
-  
+  protected 
+    def create_initial_vote
+      votes.create :user => user
+    end
 end
