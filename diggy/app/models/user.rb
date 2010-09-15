7101b3cdf55ef24de0183f765e7a068f4a0ceb34
Kamiben <devnull@localhost>
2010-09-15 10:57:48 +0200

    jointure entre user, vote et story

--- diggy/app/models/user.rb
+++ diggy/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :stories
   has_many :votes
+  has_many :stories_voted_on, # on a deja un has_many stories, alors il faut renommer l'association pour toruver les votes d'un user
+    :through => :votes, # via la table vote
+    :source => :story # on retrouve les story sur lesquelles il a voté
 end
